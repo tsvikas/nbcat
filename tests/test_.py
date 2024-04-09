0b1cc4527264ac8d68a89dc4f12edca89c679077
Tsvika S <tsvika@github>
2024-04-09 21:07:44 +0300

    add tests

--- tests/test_.py
+++
@@ -1,2 +0,0 @@
-def test_import() -> None:
-    import nbcat  # noqa: F401
---
+++ tests/test_app.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from typer.testing import CliRunner
+
+from nbcat.__main__ import SUPPORTED_FORMATS, app
+
+runner = CliRunner()
+
+
+def test_app(datadir: Path) -> None:
+    filename = datadir / "notebook.ipynb"
+    for fmt in SUPPORTED_FORMATS:
+        result = runner.invoke(
+            app, [filename.as_posix(), "-f", fmt], catch_exceptions=False
+        )
+        assert result.exit_code == 0
+        assert "success = partial" in result.stdout
+
+
+def test_app_errors(datadir: Path) -> None:
+    filename = datadir / "notebook.ipynb"
+    result = runner.invoke(app, ["MISSING_FILENAME"])
+    assert result.exit_code == 2
+    result = runner.invoke(app, [filename.as_posix(), "-f", "MISSING_FORMAT"])
+    assert result.exit_code == 11
