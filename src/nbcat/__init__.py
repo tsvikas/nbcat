36c597e53825f0f2abae2d80f38dd5fe7a6997b0
Tsvika Shapira <tsvikas@gmail.com>
2025-01-16 01:46:42 +0200

    metadata: copyright

--- src/nbcat/__init__.py
+++ src/nbcat/__init__.py
@@ -1,4 +1,7 @@
-"""Display jupyter notebooks in the terminal with syntax highlighting."""
+"""Display jupyter notebooks in the terminal with syntax highlighting.
+
+Copyright (c) 2025 Tsvika Shapira. All rights reserved.
+"""
 
 __version__ = "0.2.0+dev"
 __all__: list[str] = []
